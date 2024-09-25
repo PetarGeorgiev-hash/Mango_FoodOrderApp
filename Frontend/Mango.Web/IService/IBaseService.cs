869c51232c5d4707d1a451c2fa3946712dae2efc
Petar Georgiev <petiyo07@abv.bg>
2024-09-25 19:45:34 +0300

    added folders

--- Frontend/Mango.Web/IService/IBaseService.cs
+++ Frontend/Mango.Web/IService/IBaseService.cs
@@ -4,6 +4,7 @@ namespace Mango.Web.IService
 {
     public interface IBaseService
     {
-        Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true);
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+
     }
 }
